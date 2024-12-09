6e8e0994a49c0d0a356ac430350a52c9530fc684
enkikkeli <onni.riikonen@student.lut.fi>
2024-12-09 21:16:57 +0200

    2

---
+++ src/models/Image.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema } from "mongoose"
+
+interface IImage extends Document {
+    filename: string
+    path: string
+    createdAt: Date
+}
+
+const imageSchema = new Schema({
+    filename: {type: String, required: true},
+    path: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now}
+})
+
+const Image: mongoose.Model<IImage> = mongoose.model<IImage>("Image", imageSchema)
+
+export {Image, IImage}
