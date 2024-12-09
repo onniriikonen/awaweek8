d8d4cfaac179305196584b587be7b591918739de
enkikkeli <onni.riikonen@student.lut.fi>
2024-12-09 20:48:41 +0200

    2

--- dist/server.js
+++ dist/server.js
@@ -10,7 +10,7 @@ const index_1 = __importDefault(require("./src/index"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const port = 3000;
-const mongoDB = "mongodb://127.0.0.1:27017/offerdb";
+const mongoDB = "mongodb://127.0.0.1:27017/testdb";
 mongoose_1.default.connect(mongoDB);
 mongoose_1.default.Promise = Promise;
 const db = mongoose_1.default.connection;
